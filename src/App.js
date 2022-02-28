8ad2ce0e384b0cd25ae6c70f23afe619a3a8bb02
Mukhsinjon Bokijonov <78298971+BokijonovM@users.noreply.github.com>
2022-02-28 18:31:41 +0500

    updated

--- src/App.js
+++ src/App.js
@@ -1,32 +1,19 @@
 import "./App.css";
 import { useEffect, useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import MyHeader from "./components/MyHeader";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MyMain from "./components/MyMain";
 
-function App() {
-  const [jobs, setJobs] = useState([]);
-
-  useEffect(() => {
-    const fetchJobs = async () => {
-      try {
-        let res = await fetch("");
-      } catch (error) {
-        console.log("Error fetching", error);
-      }
-    };
-  }, []);
+const App = () => {
   return (
     <BrowserRouter>
       <div className="App">
-        <MyHeader />
         <Routes>
           <Route path="/" element={<MyMain />} />
         </Routes>
       </div>
     </BrowserRouter>
   );
-}
+};
 
 export default App;
--- src/components/MyMain.jsx
+++ src/components/MyMain.jsx
@@ -1,13 +1,88 @@
-import React from "react";
-import { Row, Col, Container } from "react-bootstrap";
+import React, { useEffect, useState } from "react";
+import { Row, Col, Container, Button } from "react-bootstrap";
+import "./style.css";
+import MyHeader from "./MyHeader";
+import Loader from "./Loader";
 
 function MyMain() {
+  const [jobs, setJobs] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [endPost, setEndPost] = useState(10);
+
+  useEffect(() => {
+    const fetchJobs = async () => {
+      try {
+        let res = await fetch("https://strive-jobs-api.herokuapp.com/jobs");
+        if (res.ok) {
+          let data = await res.json();
+          console.log(data);
+          setJobs(data);
+          setIsLoading(false);
+        } else {
+          console.log("error fetching!");
+        }
+      } catch (error) {
+        console.log("Error fetching", error);
+      }
+    };
+    fetchJobs();
+  }, []);
+
+  const showMore = (number) => {
+    if (jobs.length > 10) {
+      setEndPost(endPost + 10);
+    }
+  };
+
+  const showLess = (number) => {
+    if (endPost > 10) {
+      setEndPost(endPost - 10);
+    }
+  };
+
   return (
     <div>
+      <MyHeader />
       <Container fluid>
         <Row>
-          <Col></Col>
-          <Col></Col>
+          <Col md={6}>
+            {isLoading ? (
+              <Loader />
+            ) : (
+              jobs.data.slice(0, endPost).map((job) => {
+                return (
+                  <div className="left-jobs-div my-4 py-2 px-5" key={job.id}>
+                    <h5 className="mb-0" style={{ textAlign: "start" }}>
+                      {job.title}
+                    </h5>
+                    <p style={{ textAlign: "start" }}>{job.company_name}</p>
+                    <div className="d-flex justify-content-between align-items-center pt-3">
+                      <p className="mb-0">
+                        <span className="text-muted">Category:</span>{" "}
+                        {job.category}
+                      </p>
+                      <Button variant="secondary" size="sm">
+                        View Details
+                      </Button>
+                    </div>
+                  </div>
+                );
+              })
+            )}
+            <div className="mb-4">
+              <Button
+                className="mr-3"
+                variant="secondary"
+                onClick={(e) => showMore(+10)}
+              >
+                Show More
+              </Button>
+              <Button variant="secondary" onClick={(e) => showLess(-10)}>
+                Show Less
+              </Button>
+            </div>
+          </Col>
+          <Col md={6}>ji</Col>
         </Row>
       </Container>
     </div>
